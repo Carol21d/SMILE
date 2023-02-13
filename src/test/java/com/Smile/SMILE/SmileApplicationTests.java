9f55c8af21c10696a136049866c4ad4a50f4b3ae
Bryan <bryanpjpgi2b@hotmail.com>
2023-02-13 09:27:36 +0100

    first commit

---
+++ src/test/java/com/Smile/SMILE/SmileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Smile.SMILE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
