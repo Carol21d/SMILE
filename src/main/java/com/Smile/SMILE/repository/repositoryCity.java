f416476682de39d443e571bf1fd2838e5b4cb4f8
blake S <design.harls@icloud.com>
2023-02-16 10:08:28 +0100

    repository of entities

---
+++ src/main/java/com/Smile/SMILE/repository/repositoryCity.java
@@ -0,0 +1,9 @@
+package com.Smile.SMILE.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Smile.SMILE.models.City;
+
+public interface repositoryCity extends JpaRepository <City,Long>{
+    
+}
---
+++ src/main/java/com/Smile/SMILE/repository/repositoryProfile.java
@@ -0,0 +1,9 @@
+package com.Smile.SMILE.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Smile.SMILE.models.Profile;
+
+public interface repositoryProfile extends JpaRepository<Profile,Long>{
+    
+}
