ee9acb70efd5572b1c89ad62f619af0ab171e5e2
blake S <design.harls@icloud.com>
2023-02-28 10:29:45 +0100

    changed naming

---
+++ src/main/java/com/Smile/SMILE/services/ServiceCity.java
@@ -0,0 +1,12 @@
+package com.Smile.SMILE.services;
+
+import java.util.Optional;
+
+import antlr.collections.List;
+
+public interface ServiceCity {
+    
+  public List<City>findAll();
+  public Optional <City> find
+
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,5 @@
-INSERT INTO patients (dni,name) VALUES (1,'Bryan')
-INSERT INTO patients (dni,name) VALUES (0,'DOs')
-INSERT INTO patients (dni,name) VALUES (2,'Lol')
-INSERT INTO patients (dni,name) VALUES (3, 's')
+INSERT INTO patients (dni,name) VALUES ("1",'Bryan')
+
 -- INSERT INTO cities (id,name) VALUES (1L,'Burgos') 
 
 UPDATE patients SET  dni = 1
